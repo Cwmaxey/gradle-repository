187fc3b9a47e7dac631e590956d403572f52b1f9
Conner Maxey <connermaxey@u.boisestate.edu>
2023-03-14 19:58:25 -0600

    Changed println placement

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -56,6 +56,7 @@ public class HerokuApplication {
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
+      System.out.println("Print statement inside HerokuApplication.db() method. Conner Maxey");
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
       stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
@@ -72,8 +73,6 @@ public class HerokuApplication {
       model.put("message", e.getMessage());
       return "error";
     }
-
-    System.out.println("Print statement inside HerokuApplication.db() method. Conner Maxey");
   }
 
   @Bean
